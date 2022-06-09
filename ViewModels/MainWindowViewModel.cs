4f0f2515012c5be687651f8c3ee5d34aef387181
Pascal Groß <pg@pegasusnetworks.de>
2022-06-09 21:34:03 +0200

    Projektdateien hinzufügen.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Catel;
+using Catel.IoC;
+using Catel.MVVM;
+using Catel.Services;
+using FodyNet6.Models;
+
+namespace PartsAndMore.UI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private readonly IUIVisualizerService _uiVisualizerService;
+        public override string Title => "FodyNet6";
+
+        [Model]
+        [Expose("Name")]
+        public Person Person { get; set; }
+
+        public MainWindowViewModel(IUIVisualizerService uiVisualizerService)
+        {
+            Argument.IsNotNull(() => uiVisualizerService);
+            _uiVisualizerService = uiVisualizerService;
+        }
+        
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+        }
+
+        protected override async Task CloseAsync()
+        {
+            await base.CloseAsync();
+        }
+    }
+}
